5482b0bd58117ac954c151cb42d794ff8ddfa726
limwengni <lim1227december@gmail.com>
2024-10-01 23:32:23 +0800

    updated test py

--- test.py
+++ test.py
@@ -6,10 +6,11 @@ bert_model = BertModel.from_pretrained('bert-base-uncased')
 
 # Create a pipeline for conversational responses using Mistral 7B
 mistral_pipeline = pipeline("text-generation", model="alexsherstinsky/Mistral-7B-v0.1-sharded")
+pad_token_id = mistral_pipeline.model.config.eos_token_id
 
 def ask_question(user_input):
     # Generate a response using the Mistral pipeline
-    mistral_response = mistral_pipeline(user_input, max_length=150, num_return_sequences=1)
+    mistral_response = mistral_pipeline(user_input, max_length=450, num_return_sequences=1, truncation=True, pad_token_id=pad_token_id)
 
     # Extract the generated text
     answer = mistral_response[0]['generated_text']
